3e14c1559c5ba03baf6e979b7436b33bf109da0b
Guilherme Moreira <devguilhermemoreira@gmail.com>
2025-08-26 20:12:35 -0300

    Finalizando aba de instituições e iniciando compilado geral

--- streamlit-2025/main.py
+++ streamlit-2025/main.py
@@ -18,16 +18,41 @@ st.markdown(
 
 file_upload = st.file_uploader("Selecione um arquivo CSV", type=["csv"])
 
+# Verifica se o arquivo foi carregado
 if file_upload is not None:
+    # Lê o arquivo CSV
     df = pd.read_csv(file_upload)
     df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y").dt.date
 
+    # Visão dos dados brutos
     exp1 = st.expander("Dados brutos")
     columns_fmt = {
         "Valor": st.column_config.NumberColumn("Valor", format="R$ %.2f"),
     }
     exp1.dataframe(df, hide_index=True, column_config=columns_fmt)
 
+    # Visão das instituições
     exp2 = st.expander("Instituições")
     df_instituicao = df.pivot_table(index="Data", columns="Instituição", values="Valor")
-    exp2.dataframe(df_instituicao)
+
+    #  Abas para diferentes visualizações
+    tab_data, tab_history, tab_share = exp2.tabs(["Dados", "Histórico", "Distribuição"])
+
+    with tab_data:
+        st.dataframe(df_instituicao)
+
+    with tab_history:
+        st.line_chart(df_instituicao)
+
+    with tab_share:
+        date = st.date_input("Data para distribuição", min_value=df_instituicao.index.min(), max_value=df_instituicao.index.max())
+
+        if date not in df_instituicao.index:
+            st.warning("Entre com uma data válida")
+        else:
+            st.bar_chart(df_instituicao.loc[date])
+
+    df_date = df.groupby(by="Data")[["Valor"]].sum()
+    df_date["lag_1"] = df_date["Valor"].shift(1)
+    df_date["Diferença mensal"] = df_date["Valor"] - df_date["lag_1"]
+    st.dataframe(df_date)
